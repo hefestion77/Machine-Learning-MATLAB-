ace97ac98eb606e812af23e38ed9ab28deaa3125
hefestion77 <45935669+hefestion77@users.noreply.github.com>
2018-12-18 13:06:55 +0100

    Add files via upload

---
+++ sampledata.py
@@ -0,0 +1,30 @@
+def sampledata(X, y):
+    ''' (ndarray, ndarray) ->
+
+    '''
+    import numpy as np
+    
+    m = X.shape[0]
+    dataset = np.hstack((X,y))
+    
+    for i in range(10):
+        np.random.shuffle(dataset)
+
+    num_train_examp = int(np.floor(m * 0.6))
+    num_cv_examp = int(np.floor((m - num_train_examp) / 2))
+                     # num_test_examp = m - num_train_examp - num_cv_examp;
+
+    train_dataset = dataset[:num_train_examp]
+    cv_dataset = dataset[num_train_examp:num_train_examp + num_cv_examp]
+    test_dataset = dataset[num_train_examp + num_cv_examp:]
+
+    Xtrain = train_dataset[:,:-1]
+    ytrain = train_dataset[:,-1:]
+
+    Xval = cv_dataset[:,:-1]
+    yval = cv_dataset[:,-1:]
+
+    Xtest = test_dataset[:,:-1]
+    ytest = test_dataset[:,-1:]
+
+    return Xtrain, ytrain, Xval, yval, Xtest, ytest
