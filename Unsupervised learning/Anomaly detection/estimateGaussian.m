fdc9ea457e0fbca8848e8e4c842cfc2f720b446d
Sergio Miserachs García <45935669+hefestion77@users.noreply.github.com>
2019-01-15 12:36:03 +0100

    Update estimateGaussian.m

--- Unsupervised learning/Anomaly detection/estimateGaussian.m
+++ Unsupervised learning/Anomaly detection/estimateGaussian.m
@@ -1,1 +1,13 @@
-function [mu, sigma2] = estimateGaussian(X)% ESTIMATEGAUSSIAN This function estimates the parameters of a % Gaussian distribution using the data in X.% model must %   [mu sigma2] = estimateGaussian(X), %   The input X is the dataset with each n-dimensional data point in one row%   The output is an n-dimensional vector mu, the mean of the data set%   and the variances in the diagonal matrix sigma2% mu = mean(X); % 1 x n vectorsigma2 = var(X, 1); % variance calculated from 1/m... 1 x n vectorend
+function [mu, sigma2] = estimateGaussian(X)
+% ESTIMATEGAUSSIAN This function estimates the parameters of a 
+% Gaussian distribution using the data in X.
+
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances in the diagonal matrix sigma2
+% 
+
+mu = mean(X); % 1 x n vector
+sigma2 = var(X, 1); % variance calculated from 1/m... 1 x n vector
+
+end
