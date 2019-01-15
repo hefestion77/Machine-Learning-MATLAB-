529a5596b549c1fb4f3cc802324e265b336a6d69
Sergio Miserachs García <45935669+hefestion77@users.noreply.github.com>
2019-01-15 12:52:11 +0100

    Update linearRegCostFunction.m

--- Regression problems - Linear Regression/linearRegCostFunction.m
+++ Regression problems - Linear Regression/linearRegCostFunction.m
@@ -1,1 +1,16 @@
-function [J, grad] = linearRegCostFunction(X, y, theta, lambda)% LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear % regression with multiple variables.m = length(y); % number of training examplesThetatoReg = theta(2:end);h = X*theta;J = (1/(2*m))*(((h-y)'*(h-y)) + lambda*(ThetatoReg'*ThetatoReg));grad = (1/m)*((X'*(h-y)) + lambda*[0;ThetatoReg]);grad = grad(:);end
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+% This function computes cost and gradient for regularized linear 
+% regression with multiple variables.
+
+m = length(y); % number of training examples
+
+ThetatoReg = theta(2:end);
+
+h = X*theta;
+J = (1/(2*m))*(((h-y)'*(h-y)) + lambda*(ThetatoReg'*ThetatoReg));
+
+grad = (1/m)*((X'*(h-y)) + lambda*[0;ThetatoReg]);
+
+grad = grad(:);
+
+end
