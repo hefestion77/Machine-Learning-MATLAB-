dd6e7905636987360deadbd12241a3918c0ed90d
Sergio Miserachs García <45935669+hefestion77@users.noreply.github.com>
2019-01-15 12:34:12 +0100

    Update multivariateGaussian.m

--- Unsupervised learning/Anomaly detection/multivariateGaussian.m
+++ Unsupervised learning/Anomaly detection/multivariateGaussian.m
@@ -1,1 +1,15 @@
-function p = multivariateGaussian(X, mu, sigma2)% MULTIVARIATEGAUSSIAN Computes the probability density function of the% multivariate gaussian distribution.n = size(X, 2); % number of featuresif size(sigma2, 1) == 1 || size(sigma2, 2) == 1    sigma2 = diag(sigma2);endX = bsxfun(@minus, X, mu(:)');p = (2 * pi) ^ (- n / 2) * det(sigma2) ^ (-0.5) * ...            exp(-0.5 * sum(bsxfun(@times, X * pinv(sigma2), X), 2));      end
+function p = multivariateGaussian(X, mu, sigma2)
+% MULTIVARIATEGAUSSIAN Computes the probability density function of the multivariate gaussian distribution.
+
+n = size(X, 2); % number of features
+
+if size(sigma2, 1) == 1 || size(sigma2, 2) == 1
+    sigma2 = diag(sigma2);
+end
+
+X = bsxfun(@minus, X, mu(:)');
+
+p = (2 * pi) ^ (- n / 2) * det(sigma2) ^ (-0.5) * ...
+            exp(-0.5 * sum(bsxfun(@times, X * pinv(sigma2), X), 2));
+      
+end
